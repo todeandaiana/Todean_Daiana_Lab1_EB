e7726b6c214a44ebfee9163734de643d97a2cf50
Daiana <Daiana@DESKTOP-4N9BAGI>
2023-10-23 16:25:49 +0300

    functions

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todean_Daiana_Lab1.Controllers
+{
+    public class FirstController : Controller
+    {
+       // public IActionResult Index()
+        //{
+        //    return View();
+       // }
+
+        public String Index() {
+            return "Bine ati venit!";
+        }
+
+        public String Welcome() {
+            return "Salut, utilizatorule!";
+        }
+
+        public String Mesaj(String nume, int varsta) {
+            return $"Salut, {nume}! Aveti {varsta} ani.";
+        }
+
+    }
+}
